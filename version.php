16176e6e0a4a009398d53823aec01dc198e1142d
Georg Maißer <georg.maisser@wunderbyte.at>
2023-05-23 14:30:58 +0200

    Version 1.1.1 (2023052390)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_jack';
-$plugin->release = '1.0.4';
-$plugin->version   = 2023051000;
+$plugin->release = '1.1.0';
+$plugin->version   = 2023052390;
 $plugin->requires  = 2020060900;
 $plugin->maturity  = MATURITY_STABLE;
